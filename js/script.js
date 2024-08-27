d103c575b01761642e5278e07854203eee54cc03
abdicore <abdirhamanmohamed@gmail.com>
2024-08-27 15:48:15 +0200

    function-randomNumbers

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ Dopo 30 secondi i numeri scompaiono e appaiono invece 5 input in cui l'utente de
 precedentemente, nell'ordine che preferisce.
 Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 NOTA: non è importante l'ordine con cui l'utente inserisce i numeri, basta che ne indovini il più possibile.
-BONUS 1:
+BONUS 5:
 Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose diverse da numeri lo blocchiamo 
 in qualche modo.
 BONUS 2:
@@ -20,3 +20,26 @@ Consigli del giorno:
 indica quali e quanti numeri ci sono in comune tra i due array"
 
 */
+
+
+//Visualizzare in pagina 5 numeri casuali.
+//funzioni
+function randomNumbers() {
+    for (let i = 0; i < 5; i++) {
+        const numeroCasuale = Math.floor(Math.random() * 100) + 1;
+        console.log(numeroCasuale);
+    }
+}
+
+
+
+//Recupero elemniti dal dom
+ const numberElement = document.getElementById('numberRandom')
+ const n1 = document.getElementById('n1')
+ const n2 = document.getElementById('n2')
+ const n3 = document.getElementById('n3')
+ const n4 = document.getElementById('n4')
+ const n5 = document.getElementById('n5')
+ const buttonPlay = document.querySelector('button')
+
+ randomNumbers()
